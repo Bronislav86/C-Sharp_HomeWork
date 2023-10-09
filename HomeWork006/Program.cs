6cf6c686f2a19ba2367f375d2a1eb2e85438c01c
Bronislav <shevchenko-bg@yandex.ru>
2023-10-09 20:13:17 +0300

    added solution of task 43

--- HomeWork006/Program.cs
+++ HomeWork006/Program.cs
@@ -70,5 +70,42 @@ Console.WriteLine(Length(pointA, pointB));
 // y = k1 * x + b1
 // y = k2 * x + b2
 // k1 * x + b1 = k2 * x + b2
-// k1*x - k2*x = b1 - b2
-// x = (k1+k2)/b1 - b2
+// k1*x - k2*x = b2 - b1
+// x = (b2 - b1) / (k1-k2)
+// y = k1 * x + b1
+
+void IntersectionPoint (double b1, double k1, double b2, double k2, double x, double y)
+{
+    x = (b2 - b1) / (k1 - k2);
+    y = k1 * x + b1;
+    Console.WriteLine($"({x}; {y})");
+}
+
+Console.Clear();
+Console.WriteLine("Введите координату b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+double x = 0;
+double y = 0;
+IntersectionPoint(b1, k1, b2, k2, x, y);
+
+
+// Console.Clear();
+// Console.WriteLine("Введите координату b1: ");
+// double b1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату k1: ");
+// double k1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату b2: ");
+// double b2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату k2: ");
+// double k2 = Convert.ToInt32(Console.ReadLine());
+
+// double x = (b2 - b1) / (k1 - k2);
+// double y = (k2 * x) + b2;
+
+// Console.WriteLine($"({x}; {y})");
